80dd151ed0fbc120a40164030efdd770d2139799
Andreas Stange <anst.dev@gmail.com>
2021-10-26 16:56:14 +0200

    add DummyTest.cs

---
+++ UnityTestProject/Assets/Tests/DummyTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class DummyTest
+{
+    [Test]
+    public void DummyTestSimplePasses()
+    {
+        Debug.Log("DummyTestSimplePasses");
+    }
+
+    [Test]
+    public void DummyTestSimpleFailure()
+    {
+        Debug.Log("DummyTestSimpleFailure");
+        Assert.Fail("DummyTestSimpleFailure");
+    }
+}
