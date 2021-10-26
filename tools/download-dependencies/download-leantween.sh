1775f7f615b52c7e7b910284464e0387e347e3ba
Andreas Stange <anst.dev@gmail.com>
2021-10-26 21:01:53 +0200

    test project path with space in name

--- tools/download-dependencies/download-leantween.sh
+++ tools/download-dependencies/download-leantween.sh
@@ -2,7 +2,7 @@
 
 old_dir=$(pwd)
 
-cd "../../UnityTestProject/Assets/Plugins"
+cd "../../UnityTest Project/Assets/Plugins"
 echo "Removing old LeanTween folder..."
 rm -rf LeanTween
 mkdir LeanTween
